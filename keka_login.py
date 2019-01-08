865e0648ba9c9402f4b9b0ae7ad11049896efe0b
Karthik Varma <leovarmak@hotmail.com>
2019-01-08 17:26:47 +0530

    update code to support headless chrome, added check to see if its the first usuage

--- keka_login.py
+++ keka_login.py
@@ -2,44 +2,83 @@ import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-pwd = os.getcwd()
+# Using this to check if its the first time of the day or not
+# Default initiated with True.
+f = open("status_storage.txt", "r")
+status = f.read()
 
-browser = webdriver.Chrome(pwd + '/chromedriver')
+# instantiate a chrome options object so you can set the size and headless preference
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+# download the chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put it in the
+# current directory
+chrome_driver = os.getcwd() +"/chromedriver"
+print(chrome_driver)
 
 def keka_login():
+    global f
+    browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    print('Login Process Started: ')
     browser.get("https://discidium.keka.com/#/home")
+    print('Website Opened')
 
     time.sleep(5)
 
     email = browser.find_element_by_xpath('//*[@id="email"]')
-    email.send_keys("") # Enter your email here
+    email.send_keys("")
+    print('Email Entered')
+
 
     password = browser.find_element_by_xpath('//*[@id="password"]')
-    password.send_keys("") # Enter your password here
+    password.send_keys("")
+    print('Password Entered')
+
 
     time.sleep(5)
 
     login_button = browser.find_element_by_xpath('//*[@id="login-container-center"]/div/div/form/div/div[4]/div/button[1]')
     login_button.click()
+    print('Login Button Clicked')
+
 
     time.sleep(15)
 
     web_clockin_button = browser.find_element_by_xpath('//*[@id="attendance-widget"]/div/div[2]/div/div[1]/div[2]/input[1]')
     web_clockin_button.click()
+    print('Clicked WebClock In')
+
 
     time.sleep(5)
 
-    note_text_area = browser.find_element_by_xpath('//*[@id="ng-app"]/body/div[1]/div/div/div[2]/form/div[1]/div/textarea')
-    note_text_area.send_keys("Starting now")
 
-    time.sleep(5)
+    location_request_button = browser.find_element_by_xpath('//*[@id="ng-app"]/body/div[1]/div/div/div[3]/button')
+    location_request_button.click()
+    print('Location Request Declined')
 
-    request_button = browser.find_element_by_xpath('//*[@id="ng-app"]/body/div[1]/div/div/div[2]/form/div[2]/div/div/input[1]')
-    request_button.click()
+
+    if status == 'True':
+        note_text_area = browser.find_element_by_xpath('//*[@id="ng-app"]/body/div[1]/div/div/div[2]/form/div[1]/div/textarea')
+        note_text_area.send_keys("Starting now")
+        print('Entered Description')
+
+
+        time.sleep(5)
+
+        request_button = browser.find_element_by_xpath('//*[@id="ng-app"]/body/div[1]/div/div/div[2]/form/div[2]/div/div/input[1]')
+        request_button.click()
+        print('Clicked Request Button')
+
 
     time.sleep(15)
-
+    print('Successfully logged in')
+    f.close()
+    f = open("status_storage.txt", "w")
+    f.write("False")
+    f.close()
     browser.quit()
 
 keka_login()
--- keka_logout.py
+++ keka_logout.py
@@ -2,35 +2,69 @@ import time
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 
-pwd = os.getcwd()
-browser = webdriver.Chrome(pwd + 'chromedriver')
+# Using this to check if its the first time of the day or not
+# Default initiated with True.
+# instantiate a chrome options object so you can set the size and headless preference
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+# download the chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put it in the
+# current directory
+chrome_driver = os.getcwd() + "/chromedriver"
+print(chrome_driver)
 
 def keka_logout():
+    browser = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
+    print('Logout Process Started: ')
+
     browser.get("https://discidium.keka.com/#/home")
+    print('Website Opened')
 
     time.sleep(5)
 
     email = browser.find_element_by_xpath('//*[@id="email"]')
     email.send_keys("") # Enter your email here
+    print('Email Entered')
 
     password = browser.find_element_by_xpath('//*[@id="password"]')
     password.send_keys("") # Enter your password here
+    print('Password Entered')
+
 
     time.sleep(5)
 
     login_button = browser.find_element_by_xpath('//*[@id="login-container-center"]/div/div/form/div/div[4]/div/button[1]')
     login_button.click()
+    print('Login Button Clicked')
+
 
     time.sleep(15)
 
     web_clockout_button = browser.find_element_by_xpath('//*[@id="attendance-widget"]/div/div[2]/div/div[1]/div[2]/input')
     web_clockout_button.click()
+    print('WebClock Out Button Clicked')
 
     time.sleep(5)
 
     web_clockout_confirm_button = browser.find_element_by_xpath('//*[@id="attendance-widget"]/div/div[2]/div/div[1]/div[2]/div[2]/input[2]')
     web_clockout_confirm_button.click()
+    print('Confirmed WebClock Out')
+
+
+    time.sleep(5)
+
+    location_request_button = browser.find_element_by_xpath('//*[@id="ng-app"]/body/div[1]/div/div/div[3]/button')
+    location_request_button.click()
+    print('Rejected location request')
+
+    time.sleep(15)
+    print('Successfully logged out')
+    f = open("status_storage.txt", "w")
+    f.write("True")
+    f.close()
 
     time.sleep(20)
 
